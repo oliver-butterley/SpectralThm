3889be3c58e17dae7f71a21bccb21562e371da91
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2025-07-18 01:59:28 +0200

    Update BorelFunctionalCalculus.lean

--- SpectralThm/BorelFunctionalCalculus.lean
+++ SpectralThm/BorelFunctionalCalculus.lean
@@ -1,15 +1,32 @@
 import Mathlib
+import SpectralThm.Complex
 import SpectralThm.Resolutions
 import SpectralThm.WStarAlgebra.BorelFunctionalCalculus
 
+open InnerProductSpace ContinuousLinearMap
 
-variable (H : Type*) [NormedAddCommGroup H] [InnerProductSpace ℂ H] [CompleteSpace H]
+noncomputable section
+
+variable {H : Type*} [NormedAddCommGroup H] [InnerProductSpace ℂ H] [CompleteSpace H]
   (a : H →L[ℂ] H) (ha : IsSelfAdjoint a)
 
+variable (f : C((spectrum ℂ a), ℂ))
 
-#check a.1
-#synth ContinuousFunctionalCalculus ℝ (H →L[ℂ] H) IsSelfAdjoint
+#check (cfcHom ha.isStarNormal : C((spectrum ℂ a), ℂ) →⋆ₐ H →L[ℂ] H)
+#check cfcHom ha.isStarNormal
+
+def toLinearFunctional (x y : H) : (H →L[ℂ] H) →L[ℂ] ℂ
+    := compSL (H →L[ℂ] H) _ _ _ _ (innerSL ℂ x) (apply' H _ y)
+
+lemma toLinearFunctiona_bounded (x y : H) : ‖toLinearFunctional x y‖ ≤ ‖x‖ * ‖y‖ := by
+  sorry
 
 
-variable (f : C(ℂ, ℂ))
--- #check cfcHom ha.isStarNormal f
+/- TODO
+- `cfcHom` as continuous linear map
+- compose it with `toLinearFunctional`, obtaining a complex linear functional on
+`C((spectrum ℂ a), ℂ)`
+- define `toComplexMeasure` using ComplexRMK
+- define Borel functional calculus using `InnerProductSpace.continuousLinearMapOfBilin`
+
+-/
